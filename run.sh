2d6b708f322b5461428a251701e8c84925566bac
Javier de la Rosa <versae@gmail.com>
2020-06-24 09:52:44 +0200

    Updated script

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "Installing dependencies"
 sudo apt-get -y update
-sudo apt-get install -y jq byobu
+sudo apt-get install -y jq byobu shuf split
 pip install -qU pip
 pip install -r requirements.txt
 if [ -z "${TAG}" ]; then
@@ -18,7 +18,7 @@ if [ -z "${TAG}" ]; then
     ln -s "/share/$TAG/data" data
     ln -s "/share/$TAG/models" models
 else
-    mkdir data
+    mkdir runs
     mkdir data
     mkdir models
 fi
@@ -37,6 +37,10 @@ curl -o corpora/poesias_corpora.json -q https://raw.githubusercontent.com/linhd-
 cat corpora/poesias_corpora.json | jq -r '. | to_entries[] | select(.value) | .value.text' >> _es_train.txt
 cat _es_train.txt | awk '{$1=$1};1' | grep -v -e '^[=\*\d[:space:]]*$' | uniq > es_train.txt
 rm _es_train.txt
+shuf es_train.txt > es_data.txt
+split -l $[ $(wc -l es_data.txt|cut -d" " -f1) * 70 / 100 ] -d -a 1 --additional-suffix=.txt es_data.txt es_data.
+mv es_data.0.txt es_data.train.txt
+mv es_data.1.txt es_data.eval.txt
 mv es_* data
 
 echo "Downloading scripts"
@@ -47,7 +51,7 @@ chmod +x shutdown.sh
 
 echo "Launching jobs"
 byobu new-session -d -s "alberti" "watch -n 1 nvidia-smi"
-byobu new-window -t "alberti" "python run_language_modeling.py --output_dir=./models/alBERTi-tiny-v2 --model_type=roberta --model_name_or_path=roberta-base --do_train --train_data_file=./data/train.txt --do_eval --eval_data_file=./data/eval.txt --evaluate_during_training --save_total_limit 5 --save_steps 1000 --mlm --overwrite_output_dir --n_gpu 2"
+byobu new-window -t "alberti" "python run_language_modeling.py --output_dir=./models/alBERTi-$TAG --model_type=roberta --model_name_or_path=roberta-base --do_train --train_data_file=./data/es_data.train.txt --do_eval --eval_data_file=./data/es_data.eval.txt --evaluate_during_training --save_total_limit 5 --save_steps 1000 --mlm --overwrite_output_dir --n_gpu 2"
 byobu new-window -t "alberti" "htop"
 byobu new-window -t "alberti" "tensorboard dev upload --logdir ./runs"
 byobu new-window -t "alberti" "./shutdown.sh $(cat pid)"
