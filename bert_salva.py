f2e00206ebf6bf41866e7042267bb00f5cffe3f6
salva <sros@scc.uned.es>
2022-09-24 17:56:52 +0200

    pruebas

---
+++ bert_salva.py
@@ -0,0 +1,301 @@
+"""
+!pip install transformers
+import torch
+from torch.utils.data import TensorDataset, DataLoader, RandomSampler, SequentialSampler
+from transformers import BertTokenizer, BertForSequenceClassification
+from sklearn.model_selection import train_test_split
+
+import pandas as pd
+import numpy as np
+
+from tabulate import tabulate
+from tqdm import trange
+import random
+
+#este codigo lle un csv de dos clumnos y lo pasa adataframe y codifica los valores de la columna en 0 y 1
+def read_csv:
+    file_path = '/content/SMSSpamCollection'
+    df = pd.DataFrame({'label':int(), 'text':str()}, index = [])
+    with open(file_path) as f:
+    for line in f.readlines():
+        split = line.split('\t')
+        df = df.append({'label': 1 if split[0] == 'spam' else 0,
+                        'text': split[1]},
+                        ignore_index = True)
+    df.head()
+    text = df.text.values
+    labels = df.label.values
+
+    def preprocessisng
+    #tokenizamos
+        tokenizer = BertTokenizer.from_pretrained(
+        'bert-base-uncased',
+        do_lower_case = True
+        )
+
+# tokeniza una frase y la asigna los ids de bert.  
+def print_rand_sentence():
+  '''Displays the tokens and respective IDs of a random text sample'''
+  index = random.randint(0, len(text)-1)
+  table = np.array([tokenizer.tokenize(text[index]), 
+                    tokenizer.convert_tokens_to_ids(tokenizer.tokenize(text[index]))]).T
+  print(tabulate(table,
+                 headers = ['Tokens', 'Token IDs'],
+                 tablefmt = 'fancy_grid'))
+
+print_rand_sentence()
+
+
+# Procedemos a preparar los datos metemos los caracteres especiales 
+
+token_id = []
+attention_masks = []
+
+def preprocessing(input_text, tokenizer):
+  '''
+  Returns <class transformers.tokenization_utils_base.BatchEncoding> with the following fields:
+    - input_ids: list of token ids
+    - token_type_ids: list of token type ids
+    - attention_mask: list of indices (0,1) specifying which tokens should considered by the model (return_attention_mask = True).
+  '''
+  return tokenizer.encode_plus(
+                        input_text,
+                        add_special_tokens = True,
+                        max_length = 32,
+                        pad_to_max_length = True,
+                        return_attention_mask = True,
+                        return_tensors = 'pt'
+                   )
+#funcion para comprobar que el tokenize.encoded_plus funciona bien
+def print_rand_sentence_encoding():
+  '''Displays tokens, token IDs and attention mask of a random text sample'''
+  index = random.randint(0, len(text) - 1)
+  tokens = tokenizer.tokenize(tokenizer.decode(token_id[index]))
+  token_ids = [i.numpy() for i in token_id[index]]
+  attention = [i.numpy() for i in attention_masks[index]]
+
+  table = np.array([tokens, token_ids, attention]).T
+  print(tabulate(table, 
+                 headers = ['Tokens', 'Token IDs', 'Attention Mask'],
+                 tablefmt = 'fancy_grid'))
+
+print_rand_sentence_encoding()
+
+#codigo para crear la regla  80/20  yponerlo en formato pytorch
+val_ratio = 0.2
+# Recommended batch size: 16, 32. See: https://arxiv.org/pdf/1810.04805.pdf
+batch_size = 16
+
+# Indices of the train and validation splits stratified by labels
+train_idx, val_idx = train_test_split(
+    np.arange(len(labels)),
+    test_size = val_ratio,
+    shuffle = True,
+    stratify = labels)
+
+# Train and validation sets
+train_set = TensorDataset(token_id[train_idx], 
+                          attention_masks[train_idx], 
+                          labels[train_idx])
+
+val_set = TensorDataset(token_id[val_idx], 
+                        attention_masks[val_idx], 
+                        labels[val_idx])
+
+# Prepare DataLoader
+train_dataloader = DataLoader(
+            train_set,
+            sampler = RandomSampler(train_set),
+            batch_size = batch_size
+        )
+
+validation_dataloader = DataLoader(
+            val_set,
+            sampler = SequentialSampler(val_set),
+            batch_size = batch_size
+        )
+
+
+# TODO: CodiGO PARA CALCULAR METRICAS 
+def b_tp(preds, labels):
+  '''Returns True Positives (TP): count of correct predictions of actual class 1'''
+  return sum([preds == labels and preds == 1 for preds, labels in zip(preds, labels)])
+
+def b_fp(preds, labels):
+  '''Returns False Positives (FP): count of wrong predictions of actual class 1'''
+  return sum([preds != labels and preds == 1 for preds, labels in zip(preds, labels)])
+
+def b_tn(preds, labels):
+  '''Returns True Negatives (TN): count of correct predictions of actual class 0'''
+  return sum([preds == labels and preds == 0 for preds, labels in zip(preds, labels)])
+
+def b_fn(preds, labels):
+  '''Returns False Negatives (FN): count of wrong predictions of actual class 0'''
+  return sum([preds != labels and preds == 0 for preds, labels in zip(preds, labels)])
+
+def b_metrics(preds, labels):
+  '''
+  Returns the following metrics:
+    - accuracy    = (TP + TN) / N
+    - precision   = TP / (TP + FP)
+    - recall      = TP / (TP + FN)
+    - specificity = TN / (TN + FP)
+  '''
+  preds = np.argmax(preds, axis = 1).flatten()
+  labels = labels.flatten()
+  tp = b_tp(preds, labels)
+  tn = b_tn(preds, labels)
+  fp = b_fp(preds, labels)
+  fn = b_fn(preds, labels)
+  b_accuracy = (tp + tn) / len(labels)
+  b_precision = tp / (tp + fp) if (tp + fp) > 0 else 'nan'
+  b_recall = tp / (tp + fn) if (tp + fn) > 0 else 'nan'
+  b_specificity = tn / (tn + fp) if (tn + fp) > 0 else 'nan'
+  return b_accuracy, b_precision, b_recall, b_specificity
+view raw
+# codigo para bert calsification
+#se baja el modelo 
+# Load the BertForSequenceClassification model
+model = BertForSequenceClassification.from_pretrained(
+    'bert-base-uncased',
+    num_labels = 2,
+    output_attentions = False,
+    output_hidden_states = False,
+)
+
+# Recommended learning rates (Adam): 5e-5, 3e-5, 2e-5. See: https://arxiv.org/pdf/1810.04805.pdf
+optimizer = torch.optim.AdamW(model.parameters(), 
+                              lr = 5e-5,
+                              eps = 1e-08
+                              )
+
+# Run on GPU
+model.cuda()
+
+
+# LLAMAMOS A LA FUNCION
+if _main_
+for sample in text:
+  encoding_dict = preprocessing(sample, tokenizer)
+  token_id.append(encoding_dict['input_ids']) 
+  attention_masks.append(encoding_dict['attention_mask'])
+
+
+token_id = torch.cat(token_id, dim = 0)
+attention_masks = torch.cat(attention_masks, dim = 0)
+labels = torch.tensor(labels)
+#token_id[6]
+
+
+
+
+# TODO: TRAINING
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+# Recommended number of epochs: 2, 3, 4. See: https://arxiv.org/pdf/1810.04805.pdf
+epochs = 2
+
+for _ in trange(epochs, desc = 'Epoch'):
+    
+    # ========== Training ==========
+    
+    # Set model to training mode
+    model.train()
+    
+    # Tracking variables
+    tr_loss = 0
+    nb_tr_examples, nb_tr_steps = 0, 0
+
+    for step, batch in enumerate(train_dataloader):
+        batch = tuple(t.to(device) for t in batch)
+        b_input_ids, b_input_mask, b_labels = batch
+        optimizer.zero_grad()
+        # Forward pass
+        train_output = model(b_input_ids, 
+                             token_type_ids = None, 
+                             attention_mask = b_input_mask, 
+                             labels = b_labels)
+        # Backward pass
+        train_output.loss.backward()
+        optimizer.step()
+        # Update tracking variables
+        tr_loss += train_output.loss.item()
+        nb_tr_examples += b_input_ids.size(0)
+        nb_tr_steps += 1
+
+    # ========== Validation ==========
+
+    # Set model to evaluation mode
+    model.eval()
+
+    # Tracking variables 
+    val_accuracy = []
+    val_precision = []
+    val_recall = []
+    val_specificity = []
+
+    for batch in validation_dataloader:
+        batch = tuple(t.to(device) for t in batch)
+        b_input_ids, b_input_mask, b_labels = batch
+        with torch.no_grad():
+          # Forward pass
+          eval_output = model(b_input_ids, 
+                              token_type_ids = None, 
+                              attention_mask = b_input_mask)
+        logits = eval_output.logits.detach().cpu().numpy()
+        label_ids = b_labels.to('cpu').numpy()
+        # Calculate validation metrics
+        b_accuracy, b_precision, b_recall, b_specificity = b_metrics(logits, label_ids)
+        val_accuracy.append(b_accuracy)
+        # Update precision only when (tp + fp) !=0; ignore nan
+        if b_precision != 'nan': val_precision.append(b_precision)
+        # Update recall only when (tp + fn) !=0; ignore nan
+        if b_recall != 'nan': val_recall.append(b_recall)
+        # Update specificity only when (tn + fp) !=0; ignore nan
+        if b_specificity != 'nan': val_specificity.append(b_specificity)
+
+    print('\n\t - Train loss: {:.4f}'.format(tr_loss / nb_tr_steps))
+    print('\t - Validation Accuracy: {:.4f}'.format(sum(val_accuracy)/len(val_accuracy)))
+    print('\t - Validation Precision: {:.4f}'.format(sum(val_precision)/len(val_precision)) if len(val_precision)>0 else '\t - Validation Precision: NaN')
+    print('\t - Validation Recall: {:.4f}'.format(sum(val_recall)/len(val_recall)) if len(val_recall)>0 else '\t - Validation Recall: NaN')
+    print('\t - Validation Specificity: {:.4f}\n'.format(sum(val_specificity)/len(val_specificity)) if len(val_specificity)>0 else '\t - Validation Specificity: NaN')
+view raw
+
+#prediction
+new_sentence = 'WINNER!! As a valued network customer you have been selected to receivea £900 prize reward! To claim call 09061701461. Claim code KL341. Valid 12 hours only.'
+
+# We need Token IDs and Attention Mask for inference on the new sentence
+test_ids = []
+test_attention_mask = []
+
+# Apply the tokenizer
+encoding = preprocessing(new_sentence, tokenizer)
+
+# Extract IDs and Attention Mask
+test_ids.append(encoding['input_ids'])
+test_attention_mask.append(encoding['attention_mask'])
+test_ids = torch.cat(test_ids, dim = 0)
+test_attention_mask = torch.cat(test_attention_mask, dim = 0)
+
+# Forward pass, calculate logit predictions
+with torch.no_grad():
+  output = model(test_ids.to(device), token_type_ids = None, attention_mask = test_attention_mask.to(device))
+
+prediction = 'Spam' if np.argmax(output.logits.cpu().numpy()).flatten().item() == 1 else 'Ham'
+
+print('Input Sentence: ', new_sentence)
+print('Predicted Class: ', prediction)
+
+"""
+from sklearn import datasets, metrics
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import KFold
+from sklearn.linear_model import LogisticRegression
+import csv
+import pandas as pd
+if __name__ == '__main__':
+  pd.read_csv('training.csv')
+  X_train, X_test, y_train, y_test = train_test_split(training.Sentence, iris.Metaphor, test_size=0.2, random_state=0)
+
+  print(X_test, y_test)
